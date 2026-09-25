3d425d45cea2f9cdaa5518ea95ebb950fb019acf
Nefeli-Fernando <nefelifernando706@gmail.com>
2026-09-25 11:54:08 +0530

    DOM Document Object Manipulation

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-///////////////DOM MANIPULATION//////////////////////
 //DOM manipulation is the process of using computer code—most commonly JavaScript—to dynamically alter, add, delete, or modify the structure, content, and styling of a webpage.
 let x = 10;
 let y = 20;
@@ -18,16 +17,61 @@ class Customer{
     address;
 
     constructor(name,age,address){  //there are 2 types of constructors,       default and argument constructor. Default constructor is a constructor that does not take any parameters, while an argument constructor is a constructor that takes parameters.
-        this.name = name;
+        this.name = name;  //inject parameters
         this.age = age;
         this.address= address;
     }
 }
-let customer1 = new Customer("John doe", 23, "124 Main street");
+let customer1 = new Customer("Ichigo Kurosaki", 15, "124 Main street");
 console.log(customer1);
 
-let customer2 = new Customer("James smith", 45, "Moratuwa");
+    alert("Welcome to Soul Socity BITCH");
+
+let customer2 = new Customer("Kuchiki Rukia", 145, "Moratuwa");
 console.log(customer2);
 
 console.log(typeof customer1);
 console.log(typeof customer2);
+
+//ARRAY LISTT/////
+let Student = [
+    {
+        name: "Ichigo",
+        age:19,
+        address:"123 main street"
+    },
+    {
+        name: "Rukia",
+        age: 20,
+        address: "456 main street",
+        marks:[
+            {
+                subject:"maths",
+                mark:90,
+                subMark:[
+                {
+                substudent:"Siyath"
+                }
+            ]
+        }
+        ]
+    },
+    {
+        name: "Renji",
+        age: 21,
+        address: "789 main street"
+    },
+    {
+        name:"Yourichi",
+        age:89,
+        address:"34 HillStreet"
+    } 
+]
+console.log(Student);
+console.log(Student[1].marks[0].mark);
+console.log(Student[1].name);
+console.log(Student[2].address);
+
+console.log(document); //a detailed description of the parameters/behaviours used in ur html doc
+
+///////////////DOM MANIPULATION/////DOcument Object Model/////
